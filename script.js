0f7486482657d374bf9cff9b980343400748d961
Hayden-Haun <haydenhaun@gmail.com>
2021-08-03 16:35:55 -0600

    Changed reference variables to jQuery. Added array of objects for questions. Created function to display questions.

--- script.js
+++ script.js
@@ -1,45 +1,82 @@
-var startBtn = document.querySelector("#startBtn");
-var timerEl = document.getElementById("timer");
-var content = document.querySelector(".content");
-var questionNumber = document.querySelector(".questionNumber");
-var questions = document.querySelector(".questions");
-var answerList = document.querySelector(".answerList");
+var startBtn = $("#startBtn");
+var timerEl = $("#timer");
+var content = $(".content");
+var questionNumber = $(".questionNumber");
+var questions = $(".questions");
+
+//JQUERY VARIABLE DECLARATIONS
+var displayQuestion = $("#displayQuestion");
+var answerList = $("#answerList");
 
 var counter = 0;
 
 var questionArray = [
-  "What is the best method to iterate through something with a fixed number of iterations?",
-  "What is my name?",
-  "what is your name?",
+  {
+    question:
+      "Which programming language is not a fundamental of web development?",
+    answers: ["HTML", "C++", "CSS", "JAVASCRIPT"],
+    correctAnswer: 1,
+  },
+  {
+    question: "question2?",
+    answers: ["A", "B", "C", "D"],
+    correctAnswer: 2,
+  },
+  {
+    question: "question3?",
+    answers: ["A", "B", "C", "D"],
+    correctAnswer: 3,
+  },
 ];
 
-startBtn.addEventListener("click", function () {
-  startTimer();
-  firstQuestion();
-});
+//UPDATE EVENT LISTENER FOR JQUERY
+startBtn.on("click", startTimer);
 
 function startTimer() {
+  console.log("THIS IS WORKING!!!");
+  showNext();
   var timeLeft = 10;
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
-      timerEl.textContent = timeLeft;
+      timerEl.text(timeLeft);
       timeLeft--;
     } else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft;
+      timerEl.text(timeLeft);
       timeLeft--;
     } else {
-      timerEl.textcontent = "0";
+      timerEl.text(0);
       clearInterval(timeInterval);
       timeIsOut();
     }
   }, 1000);
 }
 
-function firstQuestion() {
-  questionNumber.textContent = "Question #1";
-  questions.textContent = "What is your name?";
+function showNext() {
+  //UPDATE TO JQUERY LINGO
+  //questionNumber.textContent = "Question # " + (counter + 1);
+  questionNumber.text("Question #" + counter);
+  var currentQuestion = questionArray[counter];
+  displayQuestion.text(currentQuestion.question);
+  for (var i = 0; i < currentQuestion.answers.length; i++) {
+    var answerLi = $(
+      `<li> <button> ${currentQuestion.answers[i]} </button> </li>`
+    );
+    answerList.append(answerLi);
+  }
+}
+
+answerList.on("click", "button", checkIfCorrect);
+
+function checkIfCorrect() {
+  //console.log("THIS IS WORKING!!!");
+
+  var selectedAnswer = $(this);
+  var correctAnswer =
+    questionArray[counter].answers[questionArray[counter].correctAnswer];
+  console.log(selectedAnswer.text());
+  console.log(correctAnswer);
 }
 
 function timeIsOut() {
-  console.log("timeIsOut working");
+  //console.log("timeIsOut working");
 }
